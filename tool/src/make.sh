d548a80adec34dd99bbfa326c22d0ca16e2feef4
canceromics <52235043+canceromics@users.noreply.github.com>
2021-08-17 17:51:33 +0800

    Delete make.sh
    change format

--- tool/src/make.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-javac -d ./ -cp lib/htsjdk-2.10.1.jar:lib/commons-math3-3.6.1.jar:lib/com.github.haifengl.smile-math-2.6.0.jar bed/*.java count/*.java exonseq/*.java genome/*.java ./main/*.java mapping/*.java sim/*.java split/*.java
-jar -cvmf MANIFEST.MF ../../diffRSS.jar *
