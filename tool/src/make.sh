6a26331755a8ea290ee299c6e4ccd4319276d18e
hasee <hasee@windows10.microdone.cn>
2022-05-10 22:49:41 +0800

    update make

--- tool/src/make.sh
+++ tool/src/make.sh
@@ -4,5 +4,5 @@ javac -d ./ -cp lib/htsjdk-2.10.1.jar:lib/commons-math3-3.6.1.jar:lib/com.github
 jar -xf lib/htsjdk-2.10.1.jar
 jar -xf lib/commons-math3-3.6.1.jar
 jar -xf lib/com.github.haifengl.smile-math-2.6.0.jar
-cp MANIFEST.MF META-INF/MANIFEST.MF
+cp ./MANIFEST.MF META-INF/MANIFEST.MF
 jar -cvmf MANIFEST.MF ../../AStruct.jar *
